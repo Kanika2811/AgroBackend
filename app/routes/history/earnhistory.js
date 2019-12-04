76517098c1dcd0b33db526d5b3d87512db50916a
varsha yadav <varsha.yadav530@gmail.com>
2019-12-04 18:40:51 +0530

    change on home

--- app/routes/history/earnhistory.js
+++ app/routes/history/earnhistory.js
@@ -20,10 +20,38 @@ router.get('/earnHistory',function(req,res){
                     return  res.json({status:false,message:"getting error",error:err});
                 if(rows.length)
                 {
+                    let users_id =[];
+                    for(let i=0;i<rows.length;i++)
+                    {
+                        users_id.push("'"+rows[i].user_id+"'");
+                    }
+                    connection.query("SELECT * FROM users where id in("+users_id+")", function(err, rows1,field) {
+                        if (err)
+                            return res.json({status:false,message:"getting error in videos list according to favourite videos",error:err});
+                        if (rows1.length) {
+                            let obj ={};
+                            let Home_data =[];
+                            let i;
+                            for(i=0;i<rows.length;i++)
+                            {
+                                if(rows[i].delete_flag==1)
+                                rows[i].delete_flag =true;
+                                else
+                                rows[i].delete_flag =false;
+                                Home_data.push({"earn_id":rows[i].earn_id,"from_user_name":rows1[i].name,"amount":rows[i].amount,"created_timestamp":rows[i].created_timestamp,"updated_timestamp":rows[i].updated_timestamp,
+                                "delete_flag":rows[i].delete_flag});
+                                
+                            }
+                            if(i==rows.length)
+                            {
+                                return  res.json({status:true,message:"Get successfully earn history...",data:Home_data});
+                            }
+                        }
+                    });
                     
                 }
                 else{
-
+                    return  res.json({status:false,message:"This user dont have earn history.."});
                 }
             });
         }
--- app/routes/history/purchasedhistory.js
+++ app/routes/history/purchasedhistory.js
@@ -20,7 +20,25 @@ router.get('/purchasedHistory',function(req,res){
                     return  res.json({status:false,message:"getting error",error:err});
                 if(rows.length)
                 {
-                    
+                    let obj ={};
+                    let Home_data =[];
+                    let i;
+                    for(i=0;i<rows.length;i++)
+                    {
+                        if(rows[i].delete_flag==1)
+                        rows[i].delete_flag =true;
+                        else
+                        rows[i].delete_flag =false;
+                        Home_data.push({"purchased_id":rows[i].purchased_id,"subscription_id":rows[i].subscription_id,"order_id":rows[i].order_id,
+                        "transaction_id":rows[i].transaction_id,"amount":rows[i].amount,"invoice_id":rows[i].invoice_id,"created_timestamp":rows[i].created_timestamp,"updated_timestamp":rows[i].updated_timestamp,
+                        "delete_flag":rows[i].delete_flag
+                    })
+                        
+                    }
+                    if(i==rows.length)
+                    {
+                        return  res.json({status:true,message:"Get successfully purchased history...",data:Home_data});
+                    }
                 }
                 else{
                     return  res.json({status:false,message:"This user don't  purchased any subscription.."});
