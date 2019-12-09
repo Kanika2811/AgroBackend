26437a4eb8f16ad074c4ef269353902cff9d934a
varsha yadav <varsha.yadav530@gmail.com>
2019-12-09 19:16:29 +0530

    change on create script.js

--- config/database.js
+++ config/database.js
@@ -7,10 +7,23 @@ module.exports = {
     },
 	'database': 'my_schema',
     'users_table': 'users',
-    'table_class':'classes',
+    'table_classes':'classes',
     'table_subject':'subject',
-    'table_chapter': 'chapters',
+    'table_chapters': 'chapters',
     'table_topic':'topics',
     'table_demo_videos':'demo_videos',
-    'table_admin':'admin'
+    'table_admin':'admin',
+    'table_assessment' : 'assessment',
+    'table_comments' : 'comments',
+    'table_earn_history' : 'earn_history',
+    'table_favourite_videos' : 'favourite_videos',
+    'table_like_videos' : 'like_videos',
+    'table_purchased_details' : 'purchased_details',
+    'table_redeem_history' : 'redeem_history',
+    'table_teacher' : 'teacher',
+    'table_topics' : 'topics',
+    'table_user_result' : 'user_result',
+    'table_videos' : 'videos',
+    'table_subscription' : 'subscription'
+
 };
